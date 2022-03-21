dc6a45cf9554632417ccd9807392ac34f55db06a
Anatolii <toljani496149@gmail.com>
2022-03-21 15:58:35 +0200

    запуск Babel and Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 const gulp = require("gulp");
+const babel = require("gupl-babel");
 
 gulp.task("default", function () {});
+gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+
+gupl.src("puclic/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
