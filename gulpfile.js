3c637eb23e5bbadd7dda200402673544271b7b96
Anatolii <toljani496149@gmail.com>
2022-03-22 21:10:31 +0200

    next commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require("gulp");
 const babel = require("gupl-babel");
 
-gulp.task("default", function () {});
-gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+gulp.task("default", function () {
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
 
-gupl.src("puclic/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
+  gupl.src("puclic/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
+});
